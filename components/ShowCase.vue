94959470465b7c30e80affcde34b38d91592966c
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-04-07 11:53:53 -0400

    Adds activities

--- components/ShowCase.vue
+++ components/ShowCase.vue
@@ -2,17 +2,19 @@
 const links = [
     { route: 'members', img: 'https://source.unsplash.com/rompecabezas-blanco-y-negro-aYPtEknQmXE' },
     { route: 'events', img: 'https://source.unsplash.com/lote-de-sillas-negras-2xaF4TbjXT0' },
+    { route: 'activities', img: 'https://unsplash.com/photos/lUaaKCUANVI/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Nzd8fHN0dWR5fGVufDB8MHx8fDE3MTI0NjkzMzN8Mg&force=true&w=640' },
     { route: 'contact', img: 'https://source.unsplash.com/teclado-de-computadora-blanco-2xU7rYxsTiM' },
+
 ]
 
 </script>
 
 <template>
-    <div class="section columns has-text-centered">
-        <div v-for="link in  links " class="column">
+    <div class="section grid is-col-min-10 is-gap-3 has-text-centered">
+        <div v-for="link in  links " class="cell">
             <NuxtLink :to="localePath(link.route)">
-                <h2>{{ $t(link.route) }}</h2>
                 <img :src="link.img" />
+                <h2>{{ $t(link.route) }}</h2>
             </NuxtLink>
         </div>
     </div>
@@ -22,7 +24,7 @@ const links = [
 h2 {
     font-family: 'Montserrat';
     font-weight: 600;
-    font-size: 2rem;
+    font-size: 1.75rem;
 }
 
 img {
