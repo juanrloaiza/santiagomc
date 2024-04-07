3b1498c2cd91644909a42ced068ca861692a3454
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-04-06 21:47:40 -0300

    Fixes navbar interactivity

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,6 +8,18 @@ const links = [
 
 </script>
 
+<script>
+
+export default {
+    data() {
+        return {
+            isActive: false,
+            showNavbar: true
+        }
+    }
+}
+</script>
+
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
@@ -15,14 +27,15 @@ const links = [
                 <h1 class="has-text-weight-bold mr-5">Santiago Mind and Cognition</h1>
             </a>
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="smcnavbar">
+            <a :aria-expanded="isActive" :class="{ 'is-active': isActive }" @click="isActive = !isActive" role="button"
+                class="navbar-burger" aria-label="menu" data-target="smcnavbar">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
             </a>
         </div>
 
-        <div id="smcnavbar" class="navbar-menu">
+        <div id="smcnavbar" class="navbar-menu" :class="{ 'is-active': isActive }">
             <div class="navbar-start">
                 <NuxtLink :to="localePath('/')" class="navbar-item">{{ $t('index') }}</NuxtLink>
                 <NuxtLink v-for="link in links" :to="localePath(link)" class="navbar-item">
@@ -36,6 +49,7 @@ const links = [
             </div>
         </div>
     </nav>
+
 </template>
 
 <style>
