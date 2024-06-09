6c8fee19c8fa581cc0b8eb7a1c2302ac439952a5
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-09 12:03:55 -0400

    Test basic SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta
+  ({
+    title: 'Santiago Mind and Cognition',
+    description: 'Somos Santiago Mind and Cognition, un grupo de investigación enfocado en filosofía de la mente y ciencia cognitiva.',
+    ogImage: 'https://unsplash.com/photos/2xU7rYxsTiM/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8a2V5Ym9hcmR8ZW58MHx8fHwxNzE3ODYzNTkyfDI&force=true&w=640'
+  })
+</script>
+
 <template>
   <NuxtPage />
 </template>
