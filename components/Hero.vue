5aebe4181cf210cf54a8a46fdde871538d7ead33
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-08 23:29:55 -0400

    Arregla el cerebro de fondo

--- components/Hero.vue
+++ components/Hero.vue
@@ -17,7 +17,7 @@ export default {
 <style scoped>
 .hero {
     background-position-y: 30% !important;
-    background: linear-gradient(rgba(19, 69, 96, 0.9), rgba(0, 0, 0, 0.9)), url('https://source.unsplash.com/una-placa-de-circuito-de-computadora-con-un-cerebro-en-ella-_0iV9LmPDn0/1920x1080');
+    background: linear-gradient(rgba(19, 69, 96, 0.9), rgba(0, 0, 0, 0.9)), url('https://unsplash.com/photos/_0iV9LmPDn0/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE3ODU0NDg2fA&force=true&w=1920');
     background-repeat: no-repeat;
     background-size: cover;
 }
