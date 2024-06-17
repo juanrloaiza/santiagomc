82e84740345b13092b416b356c55d94c7064d4bb
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-16 23:58:28 -0400

    Changes contact image to NuxtImg

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,8 +7,10 @@ const { locale } = useI18n();
 <template>
     <NuxtLayout :title="$t('contact')">
         <div class="columns">
-            <div class="column is-3 is-hidden-mobile"><img
-                    src="https://unsplash.com/photos/cCmY8YnYrGY/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTB8fHNhbnRpYWdvJTIwY2hpbGV8ZW58MHx8fHwxNzEwMzYyNjU3fDA&force=true&w=500" />
+            <div class="column is-3 is-hidden-mobile">
+                <NuxtImg
+                    src="https://unsplash.com/photos/cCmY8YnYrGY/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTB8fHNhbnRpYWdvJTIwY2hpbGV8ZW58MHx8fHwxNzEwMzYyNjU3fDA&force=true&w=500"
+                    w="200px" />
                 <p class="is-size-6 has-text-grey">Photo by <a
                         href="https://unsplash.com/photos/running-vehicles-on-road-cCmY8YnYrGY">Caio Silva</a>.</p>
             </div>
