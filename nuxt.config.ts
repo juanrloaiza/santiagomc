4093596cbe8ba3def85ba6ec37af084d64bdb912
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-17 00:05:04 -0400

    Adds favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,10 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'Santiago Mind and Cognition'
+      title: 'Santiago Mind and Cognition',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      ]
     }
   }
 })
