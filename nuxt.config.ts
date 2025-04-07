bab3a23fa935332fcd5e9a47b89489e81f9508b2
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-04-07 16:53:31 -0400

    Changes to pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,29 +2,35 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: true,
+
   modules: [
     "@nuxtjs/i18n",
     "@nuxt/content",
     "@nuxt/image",
     "dayjs-nuxt"
   ],
+
   typescript: {
     strict: false
   },
+
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es',
     strategy: 'prefix',
     vueI18n: './i18n.config.ts' // if you are using custom path, default 
   },
+
   dayjs: {
     locales: ['en', 'es'],
     plugins: ['localizedFormat']
   },
+
   image: {
     provider: 'netlify',
     domains: ['images.unsplash.com']
   },
+
   app: {
     head: {
       charset: 'utf-8',
@@ -34,5 +40,7 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
       ]
     }
-  }
+  },
+
+  compatibilityDate: '2025-04-07'
 })
