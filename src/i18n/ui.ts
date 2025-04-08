16d706c0e84195aeddbcc786eefe62fa44937651
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-04-07 21:11:47 -0400

    Adds translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,17 +1,23 @@
 export const languages = {
-    en: 'English',
-    es: 'Español',
-  };
-  
-  export const defaultLang = 'es';
-  
-  export const ui = {
-    en: {
-      'nav.home': 'Home',
-      'nav.about': 'About',
-    },
-    es: {
-      'nav.home': 'Inicio',
-      'nav.about': 'Sobre nosotros',
-    },
-  } as const;
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.members': 'Members',
+    'nav.events': 'Events',
+    'nav.reading-group': 'Reading Group',
+    'nav.wip': 'WIP Colloquium',
+  },
+  es: {
+    'nav.home': 'Inicio',
+    'nav.members': 'Miembros',
+    'nav.events': 'Eventos',
+    'nav.reading-group': 'Grupo de lectura',
+    'nav.wip': 'Coloquio WIP',
+  },
+} as const;
