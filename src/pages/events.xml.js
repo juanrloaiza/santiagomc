659c4d4fd9c1e47163e36d811d86c8fff8ce53b0
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-10-27 22:48:50 -0300

    Fixes RSS

--- src/pages/events.xml.js
+++ src/pages/events.xml.js
@@ -4,23 +4,19 @@ import { getRelativeLocaleUrl } from 'astro:i18n';
 import { marked } from 'marked';
 import {
     parseDDMMYYYY,
+    sortByYear,
     useTranslations,
 } from "../i18n/utils";
 import sanitizeHTML from "sanitize-html"
 
 export async function GET(context) {
-    const events = (await getCollection("events")).sort((a, b) => {
-        const yearA = parseDDMMYYYY(a.data.date.start).getTime();
-        const yearB = parseDDMMYYYY(b.data.date.start).getTime();
-
-        return yearB - yearA;
-    });
+    const events = (await getCollection("events")).sort(sortByYear);
 
     const t = useTranslations("es")
 
     const today = new Date()
     const upcomingEvents = events
-        .filter((e) => parseDDMMYYYY(e.data.date.start) >= today)
+        .filter((e) => parseDDMMYYYY(e.data.dates[0].date) >= today)
         .reverse();
 
     return rss({
@@ -30,14 +26,9 @@ export async function GET(context) {
         items: upcomingEvents.map(event => {
             const author = event.data.author ? `(${event.data.author})` : ""
             const title = `${t(event.data.eventType)}: ${event.data.title} ${author}`.trim()
-            const [hour, minutes] = event.data.time.start.split(":")
-            const date = parseDDMMYYYY(event.data.date.start)
-            date.setHours(hour)
-            date.setMinutes(minutes)
-            const formattedDate = `${event.data.date.start}${event.data.date.end ? ` - ${event.data.date.end}` : ""}`
+            const formattedDate = `${event.data.dates[0].date}${event.data.dates.length > 1 ? ` - ${event.data.dates[event.data.dates.length - 1].date}` : ""}`
 
-
-            const description = `${formattedDate}, ${event.data.time.start} - ${event.data.time.end}. ${event.data.place}`
+            const description = `${formattedDate} - ${event.data.place}`
 
             return {
                 title: title,
@@ -46,7 +37,6 @@ export async function GET(context) {
                 content: sanitizeHTML(marked.parse(event.body), {
                     allowedTags: sanitizeHTML.defaults.allowedTags.concat(['img'])
                 }),
-                customData: `<eventdate>${date}</eventdate>`,
             }
         }),
         customData: `<language>es-CL</language>`,
