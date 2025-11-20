5adffcdd1b7a39fa5e93c53fb5ad26badd061859
Juan R. Loaiza <juanrloaiza@gmail.com>
2025-11-20 12:24:28 -0300

    Fixes dates

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -6,8 +6,10 @@ export async function getUpcomingEvents() {
     const events = (await getCollection("events")).sort(sortByYear);
 
     const today = new Date();
+    const yesterday = new Date();
+    yesterday.setDate(today.getDate() - 1)
 
     return events
-        .filter((e) => parseDDMMYYYY(e.data.dates.slice(-1)[0].date) >= today)
+        .filter((e) => parseDDMMYYYY(e.data.dates.slice(-1)[0].date) >= yesterday)
         .reverse();
 }
