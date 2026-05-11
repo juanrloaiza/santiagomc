0bee56a6a5dc446da973c5d9825f3858e33a060f
Juan R. Loaiza <juanrloaiza@gmail.com>
2026-05-11 10:09:31 -0400

    refactor: Abstracts sorting by date

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,48 +1,47 @@
-import { ui, countries, defaultLang } from './ui';
+import { ui, countries, defaultLang } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
+  const [, lang] = url.pathname.split("/");
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
 
 export function getPageFromUrl(url: URL) {
-  const [, lang, ...page] = url.pathname.split('/');
-  return page.join('/')
+  const [, lang, ...page] = url.pathname.split("/");
+  return page.join("/");
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
-  }
+  };
 }
 
 export function useCountryTranslations(lang: keyof typeof countries) {
-  return function t(key: keyof typeof countries[typeof defaultLang]) {
+  return function t(key: keyof (typeof countries)[typeof defaultLang]) {
     return countries[lang][key] || countries[defaultLang][key];
-  }
+  };
 }
 
 export function parseDDMMYYYY(dateStr: string) {
   const match = /^(\d{2})\.(\d{2})\.(\d{4})$/.exec(dateStr);
   if (!match) return new Date(3000, 1, 1);
 
-
   const [_, day, month, year] = match;
   return new Date(Number(year), Number(month) - 1, Number(day));
 }
 
 export interface EventDate {
-  date: string,
+  date: string;
   time?: {
-    start: string,
-    end: string
-  }
+    start: string;
+    end: string;
+  };
 }
 interface EventObject {
   data: {
-    dates: Array<EventDate>
-  }
+    dates: Array<EventDate>;
+  };
 }
 
 export function sortByYear(a: EventObject, b: EventObject) {
@@ -50,4 +49,8 @@ export function sortByYear(a: EventObject, b: EventObject) {
   const yearB = parseDDMMYYYY(b.data.dates[0].date).getTime();
 
   return yearB - yearA;
+}
+
+export function sortByDate(a: { date: string }, b: { date: string }) {
+  return parseDDMMYYYY(b.date).getTime() - parseDDMMYYYY(a.date).getTime();
 }
